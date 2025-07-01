9220890af3bacef4780057d1f83363ed2b29b13a
Melek Karataş <melek022233@gmail.com>
2025-07-01 20:41:18 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿//1-n
+
+Console.WriteLine("1 den kaça kadar sayıları toplamak istiyorsunuz");
+int kacaKadar=int.Parse(Console.ReadLine());
+int orjKacaKadar=kacaKadar;
+int toplam = 0;
+while (kacaKadar > 0)
+{
+    toplam += kacaKadar;
+    kacaKadar--;
+
+}
+Console.WriteLine("1-{0} arası sayıların toplamı {1}",orjKacaKadar,toplam);
